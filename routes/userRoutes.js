8268f2084596e01b4395e190bb015a4f5fed6bf0
clarissemille <cmille749@gmail.com>
2025-01-23 17:59:59 +0100

    auth ok nav logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,5 +9,7 @@ const authController = require("../controllers/auth.controller");
 // // auth
 router.post('/register', authController.signUp);
 router.post('/login', authController.signIn);
+router.get("/logout", authController.logout);
+
 
 module.exports = router;
